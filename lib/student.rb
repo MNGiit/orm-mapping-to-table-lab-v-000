0f23cf27edc4ce1039a35c87a43ec0f9060e7c03
Mohammed Noori <kmanoorib@gmail.com>
2018-08-31 00:00:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,64 @@
 class Student
-
+  attr_accessor :name, :grade
+  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+  
+  def initialize (name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+ 
+  #def self.create_table
+  #  sql =  <<-SQL 
+  #    CREATE TABLE IF NOT EXISTS songs (
+  #      id INTEGER PRIMARY KEY, 
+  #      name TEXT, 
+  #      album TEXT
+  #      )
+  #      SQL
+  #  DB[:conn].execute(sql) 
+  #end 
+  
+  def self.create_table
+    sql =   <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+        grade TEXT
+        )
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    
+  end
+
+  #def save
+  #  sql = <<-SQL
+  #    INSERT INTO songs (name, album) 
+  #    VALUES (?, ?)
+  #  SQL
+  #
+  #  DB[:conn].execute(sql, self.name, self.album)
+  #
+  #end
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO students(name, grade)
+      VALUES (?, ?)
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade)
+    
+  end
+  
+  def create
+    
+  end
   
 end
